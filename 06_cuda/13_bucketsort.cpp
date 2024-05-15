bf29d3722d809b2bc37853089d554a8011a9410e
jcy-debug <58283632+jcy-debug@users.noreply.github.com>
2024-05-15 23:31:34 +0900

    Update 13_bucketsort.cpp

--- 06_cuda/13_bucketsort.cpp
+++ 06_cuda/13_bucketsort.cpp
@@ -1,13 +1,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
-#include <omp.h>
-#include <time.h>
-#include <iostream>
-int main() {
-  clock_t start, end;
-  start = clock();
 
+int main() {
   int n = 50;
   int range = 5;
   std::vector<int> key(n);
@@ -16,39 +11,25 @@ int main() {
     printf("%d ",key[i]);
   }
   printf("\n");
-  std::vector<int> bucket(range,0); 
-  #pragma omp parallel for shared(bucket)
-  for (int i=0; i<n; i++){
-   #pragma omp atomic
-  bucket[key[i]]++;
-  }
-  std::vector<int> offset(range,0);
-  std::vector<int> offset2(range,0);
-  // for (int i=1; i<range; i++) 
-  //   offset[i] = offset[i-1] + bucket[i-1];
+
+  std::vector<int> bucket(range); 
   #pragma omp parallel
-  for(int j=1; j<range; j<<=1)
-  {
-    #pragma omp for
-    for(int i=0; i<range; i++)
-      offset2[i] = offset[i]+bucket[i];
-    #pragma omp for
-    for(int i=j; i<range; i++)
-    offset[i] += offset2[i-j]; 
-  }
-#pragma omp parallel for
   for (int i=0; i<range; i++) {
-    int j = offset[i];
-    int q = bucket[i];
-    for (int m=q; m>0; m--) {
+    bucket[i] = 0;
+  }
+  #pragma omp for
+  for (int i=0; i<n; i++) {
+    bucket[key[i]]++;
+  }
+  #pragma omp for
+  for (int i=0, j=0; i<range; i++) {
+    for (; bucket[i]>0; bucket[i]--) {
       key[j++] = i;
     }
   }
+
   for (int i=0; i<n; i++) {
     printf("%d ",key[i]);
   }
   printf("\n");
-
-  end = clock();
-  std::cout<<"time = "<<double(end-start)/CLOCKS_PER_SEC<<"s"<<std::endl;
 }
